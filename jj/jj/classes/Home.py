85e9dd66cf18b629d0806815559c4c1313b490ec
Juan Enrique Allende <juan.allende@gmail.com>
2024-07-25 17:15:03 -0400

    v0.0.1

--- jj/jj/classes/Home.py
+++ jj/jj/classes/Home.py
@@ -3,5 +3,11 @@ class Home:
         self.name = name
         self.lastname = lastname
 
+    def __respuesta__(self):
+        return f"{self.name} " + " " + f"{self.lastname}"
+    
+
+
+
     def __str__(self):
         return f"{self.name} " + " " + f"{self.lastname}"
