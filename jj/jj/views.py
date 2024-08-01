065626f2cfb3f8499095dedb1745757b2636ca78
Juan Enrique Allende <juan.allende@gmail.com>
2024-08-01 17:15:05 -0400

    v0.0.1

--- jj/jj/views.py
+++ jj/jj/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import Template, Context
 from jj.classes.Home import Home
+from jj.settings import BASE_DIR, STATIC_ROOT,STATICFILES_DIRS
 
 
 
@@ -9,11 +10,13 @@ from jj.classes.Home import Home
 
 def index(request):
     x = Home("Juan Enrique", "Allende")
-    #favicon = STATIC_ROOT
+    #favicon = open('./static/admin/img/favicon.png')
+    pag = BASE_DIR
+    estatica = STATICFILES_DIRS
     doc_externo = open('jj/templates/index.html', 'r', encoding='utf-8')
     ptl = Template(doc_externo.read())
     doc_externo.close()
-    ctx = Context({"nombre":str(x.name),"apellido":str(x.lastname)})
+    ctx = Context({"nombre":str(x.name),"apellido":str(x.lastname),"pagina":pag, "estatica":estatica})
     documento = ptl.render(ctx)    
     return HttpResponse(documento)
 
