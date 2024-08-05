e2815c69a0735476b7d9a982454694177eed8054
Juan Enrique Allende <juan.allende@gmail.com>
2024-08-05 16:27:31 -0400

    v0.0.1

--- jj/jj/settings.py
+++ jj/jj/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'jj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'jj' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- jj/jj/views.py
+++ jj/jj/views.py
@@ -1,24 +1,17 @@
 from django.http import HttpResponse
 from django.template import Template, Context
 from jj.classes.Home import Home
-from jj.settings import BASE_DIR, STATIC_ROOT,STATICFILES_DIRS
-
+from jj.settings import BASE_DIR
+from django.template import loader
+from django.shortcuts import render
 
 
 
 
 
 def index(request):
-    x = Home("Juan Enrique", "Allende")
-    #favicon = open('./static/admin/img/favicon.png')
-    pag = BASE_DIR
-    estatica = STATICFILES_DIRS
-    doc_externo = open('jj/templates/index.html', 'r', encoding='utf-8')
-    ptl = Template(doc_externo.read())
-    doc_externo.close()
-    ctx = Context({"nombre":str(x.name),"apellido":str(x.lastname),"pagina":pag, "estatica":estatica})
-    documento = ptl.render(ctx)    
-    return HttpResponse(documento)
+    x = Home("Juan Enrique", "Allende")       
+    return render(request, "index.html", {"nombre":str(x.name),"apellido":str(x.lastname)})
 
 
 
