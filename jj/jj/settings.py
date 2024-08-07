2a1d22b989b983fa7b280e8199c081d1194d389b
Juan Enrique Allende <juan.allende@gmail.com>
2024-08-07 18:00:34 -0400

    v0.0.1

--- jj/jj/settings.py
+++ jj/jj/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+import django.contrib.staticfiles 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,8 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR / 'jj' / 'static')
-STATICFILES_DIRS=[BASE_DIR / "static",]
+STATIC_ROOT = os.path.join(BASE_DIR, 'D:/mio/DEV/jj/web/jj/jj/static/')
+STATICFILES_DIRS=[os.path.join(BASE_DIR, "")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
